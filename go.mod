4f6dfbfc2424911080821761e9322a10ece766ca
diamondburned <datutbrus@gmail.com>
2020-10-27 13:42:48 -0700

    bumped to cchat v0.3.11

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.14
 require (
 	github.com/Pallinder/go-randomdata v1.2.0
 	github.com/diamondburned/aqs v0.0.0-20200704043812-99b676ee44eb
-	github.com/diamondburned/cchat v0.3.8
+	github.com/diamondburned/cchat v0.3.11
 	github.com/lucasb-eyer/go-colorful v1.0.3
 	github.com/pkg/errors v0.9.1
 	golang.org/x/text v0.3.3 // indirect
--- internal/service/authenticator.go
+++ internal/service/authenticator.go
@@ -4,12 +4,21 @@ import (
 	"github.com/diamondburned/cchat"
 	"github.com/diamondburned/cchat-mock/internal/internet"
 	"github.com/diamondburned/cchat-mock/internal/session"
+	"github.com/diamondburned/cchat/text"
 	"github.com/pkg/errors"
 )
 
 type Authenticator struct{}
 
 var _ cchat.Authenticator = (*Authenticator)(nil)
+
+func (Authenticator) Name() text.Rich {
+	return text.Plain("Slow Authentication")
+}
+
+func (Authenticator) Description() text.Rich {
+	return text.Plain("")
+}
 
 func (Authenticator) AuthenticateForm() []cchat.AuthenticateEntry {
 	return []cchat.AuthenticateEntry{
@@ -27,18 +36,27 @@ func (Authenticator) AuthenticateForm() []cchat.AuthenticateEntry {
 	}
 }
 
-func (Authenticator) Authenticate(form []string) (cchat.Session, error) {
+func (Authenticator) Authenticate(form []string) (cchat.Session, cchat.AuthenticateError) {
 	// SLOW IO TIME.
-	if err := internet.SimulateAustralian(); err != nil {
-		return nil, errors.Wrap(err, "Authentication failed")
+	err := internet.SimulateAustralian()
+	if err == nil {
+		return session.New(form[0], ""), nil
 	}
 
-	return session.New(form[0], ""), nil
+	return nil, cchat.WrapAuthenticateError(errors.Wrap(err, "Authentication failed"))
 }
 
 type FastAuthenticator struct{}
 
 var _ cchat.Authenticator = (*FastAuthenticator)(nil)
+
+func (FastAuthenticator) Name() text.Rich {
+	return text.Plain("Fast Authenticator")
+}
+
+func (FastAuthenticator) Description() text.Rich {
+	return text.Plain("Internet fails and slow-downs disabled.")
+}
 
 func (FastAuthenticator) AuthenticateForm() []cchat.AuthenticateEntry {
 	return []cchat.AuthenticateEntry{
@@ -48,6 +66,6 @@ func (FastAuthenticator) AuthenticateForm() []cchat.AuthenticateEntry {
 	}
 }
 
-func (FastAuthenticator) Authenticate(form []string) (cchat.Session, error) {
+func (FastAuthenticator) Authenticate(form []string) (cchat.Session, cchat.AuthenticateError) {
 	return session.New(form[0], ""), nil
 }
